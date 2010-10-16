d80534b7e104995a25a40c9d6fa70068e5f309a9
jessieangles <jessie@angelmg.com>
2010-10-15 17:49:31 -0700

    Initial commit to xoxo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xoxo::Application.config.secret_token = '0d098091759f2b7aa2b5945f504a995d80a349afcee51bc68a9f9e993f56e59a72def3331a5ec3c64d23fd9826ffe6c38f50bf4f59ece15b4190246239bc2a59'
